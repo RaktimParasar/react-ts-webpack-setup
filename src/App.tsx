f04cb6e890adc52adcaaa2ad30a227c60584eb7c
Raktim Parasar <raktimparasar96@gmail.com>
2023-07-08 14:45:51 +0530

    react root & render setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>React + Typescript</h1>
+      <p>A starter setup for React with TypeScript and Webpack. </p>
+    </div>
+  );
+};
+
+export default App;
