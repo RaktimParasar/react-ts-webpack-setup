3ee217a5fdd29a23a272e2eecdc5ddd7342984f9
Raktim Parasar <raktimparasar96@gmail.com>
2023-07-11 11:41:38 +0530

    setting up prod and dev env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import "./App.css";
+import reactlogo from "./assets/react.svg";
+import tslogo from "./assets/typescript.svg";
 
 const App = () => {
   return (
-    <div>
-      <h1>React + Typescript</h1>
-      <p>A starter setup for React with TypeScript and Webpack. </p>
-    </div>
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactlogo} className="logo" alt="React logo" />
+        </a>
+        <a href="" target="_blank">
+          <img src={tslogo} className="logo" alt="Typescript logo" />
+        </a>
+      </div>
+      <div>
+        <h1>React + Typescript</h1>
+        <p>A starter setup for React with TypeScript and Webpack. </p>
+      </div>
+      <div>
+        {process.env.NODE_ENV} {process.env.name}
+      </div>
+    </>
   );
 };
 
