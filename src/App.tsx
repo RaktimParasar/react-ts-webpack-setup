671fbaa2c42a92bbce28f787d9cd7d2b8677af85
Raktim Parasar <raktimparasar96@gmail.com>
2023-07-11 12:48:11 +0530

    test react-refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 import "./App.css";
 import reactlogo from "./assets/react.svg";
 import tslogo from "./assets/typescript.svg";
 
 const App = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <>
       <div>
@@ -20,6 +28,7 @@ const App = () => {
       <div>
         {process.env.NODE_ENV} {process.env.name}
       </div>
+      <button onClick={handleCount}>count {count}</button>
     </>
   );
 };
